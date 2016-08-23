d0f9a7a46a4c4e30bc865dfd0825508099d5ac81
Aries0d0f <aries0d0f@gmail.com>
2016-08-24 05:50:09 +0800

    MSM

---
+++ msm/assets/js/script.js
@@ -0,0 +1,29 @@
+$(function(){
+	
+	var note = $('#note'),
+		ts = new Date(2017, 1, 20),
+		newYear = true;
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " 天" + ( days==1 ? '':'' ) + " ｜ ";
+			message += hours + " 小時" + ( hours==1 ? '':'' ) + " ｜ ";
+			message += minutes + " 分鐘" + ( minutes==1 ? '':'' ) + " ｜ ";
+			message += seconds + " 秒" + ( seconds==1 ? '':'' ) + " <br /><br />";
+			
+			if(days!=0){
+				message += "2017梅樹月於3/25，邀請您一同共襄盛舉";
+			}
+			else {
+				message += "2017梅樹月於今日舉辦，邀請您一同共襄盛舉";
+			}
+			
+			note.html(message);
+		}
+	});
+	
+});
