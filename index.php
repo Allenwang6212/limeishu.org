508eb7b73de6678aec6b60d7c978ce9c4e124dbc
aries0d0f <aries0d0f@gmail.com>
2016-10-18 23:11:50 +0800

    Alpha5.0

--- index.php
+++ index.php
@@ -1,107 +1,52 @@
 <!DOCTYPE HTML>
-<?php
-include(a3-lazy-load/a3-lazy-load.php);
-?>
+<!--
+
+-->
 <html>
 	<head>
-	    <link rel="preload" href="assets/css/launguage.css" as="style" onload="this.rel='stylesheet'"/>
-        <link rel="preload" href="assets/css/font-awesome.min.css" as="style" onload="this.rel='stylesheet'"/>
-        <link rel="preload" href="assets/css/font.css" as="style" onload="this.rel='stylesheet'"/>
-        <link rel="preload" href="assets/css/reset.css" as="style" onload="this.rel='stylesheet'"/>
 		<title>李梅樹紀念館 - The Li Mei-shu Memorial Gallery</title>
-		<link rel="stylesheet" href="assets/css/reset.css" />
+
 		<meta charset="utf-8" />
 		<link rel="alternate" hreflang="x" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="Cache-Control" content="no-cache" />
-        <link rel="stylesheet" href="assets/css/font.css" />
         <body>
-            <link rel="stylesheet" href="assets/css/launguage.css" />
-            <style>
-                #banner {
-		            background-attachment: fixed;
-                    background-color: #2c2721;
-                    background-image: url(images/BG4.jpg);
-                    background-position: center center;
-                    background-size: cover;
-                    box-shadow: 0 0.25em 0.5em 0 rgba(0, 0, 0, 0.25);
-                    text-align: center;
-                    padding: 1em;
-                    animation: 20s pic ease-in-out infinite;
-                    -webkit-animation: 20s pic ease-in-out infinite;
-                    -ms-animation: 20s pic ease-in-out infinite;
-	           }
-            </style>
+                <!-- 首頁 -->
 				<section id="banner">
 					<div class="content">
 						<header>
 							<h2>李梅樹紀念館</h2>
                             <p>The Li Mei-shu Memorial Gallery</p>
                             <p>用藝術，說土地的故事</p>
-                            <style>
-                                .warning {
-                                    text-align: center;
-                                    display: block;
-                                    border: .001em;
-	                                border-radius: .5em;
-                                    border-color: #fff;
-                                    border-style: solid;
-                                    padding: .5em;
-                                }
-                            </style>
-                            <p class="warning">
-                                本網站正在整新開發中，期待帶給您更好的服務<br>
-                                舊有網站請至<a href="http://limeishu.org">這裡</a>
-                            </p>
                             <hr>
-						    <a href="tw/" class="button">中文</a>
-                            <a href="en-us/index.html" class="button">English</a>
-                            <a href="jp/index.html" class="button">日本語</a>
+						    <a href="introduction.htm" class="button">中文</a>
+                            <a href="anglais/anglais.htm" class="button">English</a>
+                            <a href="nihongo/nihongo.htm" class="button">日本語</a>
 						</header>
 						<header>
 						    <span class="image">
 						    <img src="assets/css/images/hend.jpg"></span>
 						</header>
-						
+
 					</div>
-                </section>
-        <footer id="footer">
-            <ul class="icons">
-                <li><a href="tel:0226732333" class="icon alt fa-phone" title="電話聯絡紀念館"><span class="label">Phone</span></a></li>
-                <li><a href="tel:0226736077" class="icon alt fa-fax" title="傳真給紀念館"><span class="label">FAX</span></a></li>
-                <li><a href="https://www.facebook.com/limeishu.org/" class="icon alt fa-facebook" title="Li Mei-Shu Memorial Gallery (李梅樹紀念館)"><span class="label">Facebook</span></a></li>
-                <li><a href="mailto:lms@limeishu.org" class="icon alt fa-envelope" title="寄信給紀念館"><span class="label">Email</span></a></li>
-                <li><a href="https://github.com/ZLCSC" class="icon alt fa-github" title="取得紀念館網站原始碼"><span class="label">GitHub</span></a></li>
-            </ul>
-            <ul class="copyright">
-                <li>&copy; 2016. 李梅樹紀念館 Li Mei-shu Memorial Gallery.</li><li> Design: LIT Team</li>
-				<li>本著作係採用<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">創用 CC BY-NC-ND 4.0 國際授權條款</a>授權.</li>
-				<li>建議使用<a href="https://www.mozilla.org">Firefox瀏覽器</a></li>
-            </ul>
-        </footer>
-        <script>
-            var loadDeferredStyles = function() {
-            var addStylesNode = document.getElementById("deferred-styles");
-            var replacement = document.createElement("div");
-            replacement.innerHTML = addStylesNode.textContent;
-            document.body.appendChild(replacement)
-            addStylesNode.parentElement.removeChild(addStylesNode);
-            };
-            var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-                webkitRequestAnimationFrame || msRequestAnimationFrame;
-            if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-            else window.addEventListener('load', loadDeferredStyles);
-        </script>
+          </section>
+								<footer id="footer">
+				            <ul class="icons">
+				                <li><a href="tel:0226732333" class="icon alt fa-phone" title="電話聯絡紀念館"><span class="label">Phone</span></a></li>
+				                <li><a href="tel:0226736077" class="icon alt fa-fax" title="傳真給紀念館"><span class="label">FAX</span></a></li>
+				                <li><a href="https://www.facebook.com/limeishu.org/" class="icon alt fa-facebook" title="Li Mei-Shu Memorial Gallery (李梅樹紀念館)"><span class="label">Facebook</span></a></li>
+				                <li><a href="mailto:lms@limeishu.org" class="icon alt fa-envelope" title="寄信給紀念館"><span class="label">Email</span></a></li>
+				                <li><a href="https://github.com/ZLCSC" class="icon alt fa-github" title="取得紀念館網站原始碼"><span class="label">GitHub</span></a></li>
+				            </ul>
+				            <ul class="copyright">
+				                <li>&copy; 2016. 李梅樹紀念館 Li Mei-shu Memorial Gallery.</li><li> Design: LIT Team</li>
+								<li>本著作係採用<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">創用 CC BY-NC-ND 4.0 國際授權條款</a>授權.</li>
+								<li>建議使用<a href="https://www.mozilla.org">Firefox瀏覽器</a></li>
+				            </ul>
+				        </footer>
 	</body>
 </html>
-<script type="text/javascript" src="tw/assets/js/jquery.min.js" async></script>
-<script type="application/javascript">
-    (function(a){a.preload=function(){var d=Object.prototype.toString.call(arguments[0])==="[object Array]"?arguments[0]:arguments;
-    var c=[];var b=d.length;for(;b--;){c.push(a("<img />").attr("src",d[b]));}};})(jQuery);
-    $.preload(
-               'assets/css/images/BG1.jpg',
-               'assets/css/images/BG2.jpg',
-               'assets/css/images/BG3.jpg',
-               'assets/css/images/BG4.jpg'
-             );
-</script>
+<link rel="stylesheet" href="assets/css/reset.css" />
+<link rel="stylesheet" href="assets/css/launguage.css" />
+<link rel="stylesheet" href="assets/css/size.css" />
+<link rel="stylesheet" href="assets/css/font.css" />
