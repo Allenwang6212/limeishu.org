5f90918a028a4ab735239dee1bb253b52df8f71d
Aries Cs <aries0d0f@gmail.com>
2016-10-29 12:23:12 +0800

    caht api test

--- chat/db.js
+++ chat/db.js
@@ -1,11 +1,9 @@
-var mongoose = require( 'mongoose' );
-var Schema   = mongoose.Schema;
-
-var Todo = new Schema({
-    user_id    : String,
-    content    : String,
-    updated_at : Date
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Message = new Schema({
+    user_id: String
+    , content: String
+    , updated_at: Date
 });
-
-mongoose.model( 'Server', Server );
-mongoose.connect( 'mongodb://localhost/express-todo' );
+mongoose.model('Messange', Messange);
+mongoose.connect('mongodb://localhost/express-messange');
