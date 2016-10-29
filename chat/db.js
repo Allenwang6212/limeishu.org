1edfbd5d084aab0191a2866f3f6c70a5b4e1c235
Aries0d0f <aries0d0f@gmail.com>
2016-10-29 12:27:20 +0800

    Alpha 5.3

--- chat/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var Message = new Schema({
-    user_id: String
-    , content: String
-    , updated_at: Date
-});
-mongoose.model('Messange', Messange);
-mongoose.connect('mongodb://localhost/express-messange');
