d98b10fdf689132de933c1a87b787bc6fdf9b669
Aries Cs <aries0d0f@gmail.com>
2016-11-06 16:49:08 +0800

    fix bandit's bug

---
+++ tw/assets/js/header.js
@@ -0,0 +1,11 @@
+var header = new Vue({
+    el: '#header-nav',
+    data: {
+        lists: [
+            { content: '<a href="#">文化活動</a>' },
+            { content: '<a href="#">關於我們</a>' },
+            { content: '<a href="#">李梅樹</a>' },
+            { content: '<a href="#">數位館藏</a>' }
+        ]
+    }
+})
