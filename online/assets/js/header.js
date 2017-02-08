fc6d1053460e352471397bd3ea1edae497d7e92e
aries0d0f <aries0d0f@gmail.com>
2017-02-08 15:01:02 +0800

    fix link

---
+++ online/assets/js/header.js
@@ -0,0 +1,5 @@
+var header = document.getElementById("header");
+var logo = '/images/Icon/Logo/Logo.png';
+var content = '<h1 id="logo">李梅樹紀念館</h1><img id="logo" src="' + logo + '" alt=""><span class="toggle"><i class="fa fa-bars" aria-hidden="true"></i></span><nav id="nav"> <ul> <li><a href="#">人物之美</a> </li><li><a href="#">風景之美</a> </li><li><a href="#">靜物之美</a> </li><li><a href="#">動物之美</a> </li><li><a href="https://limeishu.org.tw">回首頁</a></li></ul></nav>';
+
+header.innerHTML = content;
---
+++ online/assets/js/main.js
@@ -0,0 +1,60 @@
+$(function () {
+  //scroll down
+  $(".scrolly").bind('click', function (event) {
+    event.preventDefault(); //先取消超連結原本預設動作
+    var $anchor = $(this);
+    $('body').animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1000, 'easeInOutQuint');
+  });
+});
+//    traffic slide
+$('.msg').hide();
+$('.choose').click(function () {
+  $('.msg').hide("slow");
+  $('.choose').removeClass("active");
+  var choosed = $(this).data('type');
+  $('.msg[data-type=' + choosed + ']').show("slow");
+  $('.choose[data-type=' + choosed + ']').addClass("active");
+})
+$('.choose[data-type=rail]').trigger("click");
+$('.choose[data-type=info]').trigger("click");
+$('.choose[data-type=source]').trigger("click");
+$('.choose[data-type=self]').trigger("click");
+//news
+$('.all-news').slick({
+  dots: true
+  , infinite: true
+  , speed: 500
+  , slidesToShow: 3
+  , slidesToScroll: 1, // variableWidth:true,
+  responsive: [{
+    breakpoint: 1024
+    , settings: {
+      slidesToShow: 3
+      , slidesToScroll: 3
+      , infinite: true
+      , dots: true
+    }
+      }, {
+    breakpoint: 600
+    , settings: {
+      slidesToShow: 2
+      , slidesToScroll: 2
+    }
+      }, {
+    breakpoint: 480
+    , settings: {
+      slidesToShow: 1
+      , slidesToScroll: 1
+    }
+      }]
+});
+//  navbar
+$(document).ready(function () {
+  $(".toggle").click(function () {
+    $(this).toggleClass("active");
+    $(".nav").slideToggle();
+    $("#nav").toggleClass("active");
+  });
+});
