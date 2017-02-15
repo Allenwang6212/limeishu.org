10a494e47fbf569a459d49f68956cc7a1e86af80
aries0d0f <aries0d0f@gmail.com>
2017-02-15 17:54:47 +0800

    Fix SEO Script UA Error

--- assets/js/seo.js
+++ assets/js/seo.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+        $.ajaxSetup({
+                beforeSend: function (request) {
+                        request.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246");
+                }
+        });
         $.ajax({
                 url: 'https://www.google.com/search?q=limeishu.org.tw&rlz=1C5CHFA_enTW720TW720&oq=limeishu.org.tw&aqs=chrome..69i58j69i60l3j69i57.3111j0j1&sourceid=chrome&ie=UTF-8'
         });
@@ -6,6 +11,6 @@ $(document).ready(function () {
                 url: 'https://www.google.com.tw/search?q=site:limeishu.org.tw&gws_rd=cr,ssl&ei=ytyjWLvLFYn68gXyrJXQAQ'
         });
         $.ajax({
-                url: 'https://www.google.com.tw/search?q=李梅樹紀念館&gws_rd=cr,ssl&ei=ytyjWLvLFYn68gXyrJXQAQ'
+                url: 'https://www.google.com.tw/search?q=site:limeishu.org.tw&gws_rd=cr,ssl&ei=ytyjWLvLFYn68gXyrJXQAQ'
         });
 })
