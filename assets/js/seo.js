f9aabb715485bdacb0b9315d8a723b2b22fa017e
aries0d0f <aries0d0f@gmail.com>
2017-02-15 12:46:11 +0800

    add more seo script

--- assets/js/seo.js
+++ assets/js/seo.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
         $.ajax({
                 url: 'https://www.google.com/search?q=limeishu.org.tw&rlz=1C5CHFA_enTW720TW720&oq=limeishu.org.tw&aqs=chrome..69i58j69i60l3j69i57.3111j0j1&sourceid=chrome&ie=UTF-8'
         });
+        $.ajax({
+                url: 'https://www.google.com.tw/search?q=site:limeishu.org.tw&gws_rd=cr,ssl&ei=ytyjWLvLFYn68gXyrJXQAQ'
+        });
 })
