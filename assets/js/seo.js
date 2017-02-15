7447f5cebe7afc3feb3d38aac190cb881fbeac99
aries0d0f <aries0d0f@gmail.com>
2017-02-15 12:38:15 +0800

    fix

--- assets/js/seo.js
+++ assets/js/seo.js
@@ -1,3 +1,5 @@
-$.ajax({
-        url: 'https://www.google.com/search?q=limeishu.org.tw&rlz=1C5CHFA_enTW720TW720&oq=limeishu.org.tw&aqs=chrome..69i58j69i60l3j69i57.3111j0j1&sourceid=chrome&ie=UTF-8'
-});
+$(document).ready(function () {
+        $.ajax({
+                url: 'https://www.google.com/search?q=limeishu.org.tw&rlz=1C5CHFA_enTW720TW720&oq=limeishu.org.tw&aqs=chrome..69i58j69i60l3j69i57.3111j0j1&sourceid=chrome&ie=UTF-8'
+        });
+})
