b240b72d26eed19d299fcf23c368315216a640e8
aries0d0f <aries0d0f@gmail.com>
2017-03-07 19:59:23 +0800

    msm 2017 init

--- assets/js/seo.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-        $.get("https://www.google.com.tw/search?q=site:limeishu.org.tw", function (data) {});
-        $.get("https://www.google.com/search?q=www.limeishu.org.tw", function (data) {});
-})
